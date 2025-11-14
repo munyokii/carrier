90527808673d99db87db8ebaecfec14c16cec476
munyokii <munyoki912@gmail.com>
2025-11-14 17:38:07 +0300

    Created splash screen for app start point

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+
+class OnboardingContent {
+  final String title;
+  final String description;
+  final String image;
+
+  OnboardingContent({
+    required this.title,
+    required this.description,
+    required this.image,
+  });
+}
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  int _currentIndex = 0;
+  late PageController _controller;
+
+  final Color _primaryColor = const Color(0xFFFF6E00);
+
+  final List<OnboardingContent> contents = [
+    OnboardingContent(
+      title: 'Easily track your\npackage',
+      description: 'Getting location updates from our\ncarriers',
+      image: 'assets/images/screen1.webp',
+    ),
+    OnboardingContent(
+      title: "Fast delivery in\nyour area",
+      description: "Your package will come right to\nyour door as soon as possible",
+      image: 'assets/images/screen2.webp',
+    ),
+    OnboardingContent(
+      title: "Delivering your\npackage carefully",
+      description: "We care about your package as\nyou do",
+      image: 'assets/images/screen3.webp',
+    ),
+  ];
+
+  @override
+  void initState() {
+    _controller = PageController(initialPage: 0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: PageView.builder(
+                controller: _controller,
+                itemCount: contents.length,
+                onPageChanged: (int index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                itemBuilder: (_, i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Expanded(
+                          flex: 2,
+                          child: Container(
+                            alignment: Alignment.bottomCenter,
+                            // USE Image.asset(...) HERE
+                            child: Image.asset(
+                              contents[i].image,
+                              height: 300,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 40),
+                        Text(
+                          contents[i].title,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w800,
+                            height: 1.2,
+                            color: Colors.black,
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        Text(
+                          contents[i].description,
+                          textAlign: TextAlign.center,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            color: Colors.grey,
+                            height: 1.5,
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: List.generate(
+                contents.length,
+                (index) => buildDot(index, context),
+              ),
+            ),
+
+            Container(
+              height: 60,
+              margin: const EdgeInsets.all(40),
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_currentIndex == contents.length - 1) {
+                    // TODO: add navigation to Login Screen here
+                    print("Navigate to Next Screen");
+                  } else {
+                    _controller.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn,
+                    );
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: _primaryColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                  elevation: 0,
+                ),
+                child: const Text(
+                  "Next",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: Colors.white
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildDot(int index, BuildContext context) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 300),
+      height: 8,
+      width: _currentIndex == index ? 30 : 8,
+      margin: const EdgeInsets.only(right: 5),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        color: _currentIndex == index ? _primaryColor : Colors.grey.shade300,
+      ),
+    );
+  }
+}

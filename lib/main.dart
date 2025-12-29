5e7365c33f6f1e0c9f40a4716daec9e902e40e24
munyokii <munyoki912@gmail.com>
2025-12-29 22:43:19 +0300

    fIXING APP CHECK WARNING

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,18 @@ import 'screens/logo_splash.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:firebase_app_check/firebase_app_check.dart';
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
+  
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
+  );
+  
+  await FirebaseAppCheck.instance.activate(
+    androidProvider: AndroidProvider.debug,
+    appleProvider: AppleProvider.debug,
   );
   
   runApp(const CarrierApp());
